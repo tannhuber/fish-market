2ff05129f48afd896b45f444b57dda7cf5b8b3a7
Joseph <sepp.tannhuber@yahoo.de>
2015-06-05 18:46:16 +0200

    Modify md2epub

--- authoring/md2epub/md2epub.fish
+++ authoring/md2epub/md2epub.fish
@@ -53,21 +53,22 @@ end
 ################################################################################
 set tmpfile1 (mktemp)
 set tmpfile2 (mktemp)
-grep --color=never '^#[^#]' (seq 0 9)*.md > $tmpfile1; and set uppertocdepth 1
-grep --color=never '^##[^#]' (seq 0 9)*.md >> $tmpfile1; and set uppertocdepth 2
-grep --color=never '^###[^#]' (seq 0 9)*.md >> $tmpfile1; and set uppertocdepth 3
-grep --color=never '^####[^#]' (seq 0 9)*.md >> $tmpfile1; and set uppertocdepth 4
+grep --color=never '^#[^#]' (seq 0 9)*.md > /dev/null; and set uppertocdepth 1
+grep --color=never '^##[^#]' (seq 0 9)*.md > /dev/null; and set uppertocdepth 2
+grep --color=never '^###[^#]' (seq 0 9)*.md > /dev/null; and set uppertocdepth 3
+grep --color=never '^####[^#]' (seq 0 9)*.md > /dev/null; and set uppertocdepth 4
 grep --color=never '^####[^#]' (seq 0 9)*.md > /dev/null; and set lowertocdepth 4
 grep --color=never '^###[^#]' (seq 0 9)*.md > /dev/null; and set lowertocdepth 3
 grep --color=never '^##[^#]' (seq 0 9)*.md > /dev/null; and set lowertocdepth 2
 grep --color=never '^#[^#]' (seq 0 9)*.md > /dev/null; and set lowertocdepth 1
-sed -i -e 's|\.md:|\\t|' -e 's|####|4\\t|' -e 's|###|3\\t|' -e 's|##|2\\t|' -e 's|#|1\\t|' $tmpfile1
-sort $tmpfile1 | nl -n ln > $tmpfile2
-rm $tmpfile1
-set references (cut -f1 $tmpfile2 | sed 's/[[:space:]]*$//' )
-set contentfiles (cut -f2 $tmpfile2)
-set sectiondepths (cut -f3 $tmpfile2)
-set sections (cut -f4 $tmpfile2 | sed 's|^[[:space:]]*||')
+for file in (seq 0 9)*.md
+    grep -H --color=never '^#' $file \
+    | sed -e 's|\.md:|\\t|' -e 's|####|4\\t|' -e 's|###|3\\t|' -e 's|##|2\\t|' -e 's|#|1\\t|' >> $tmpfile2
+end
+set contentfiles (cut -f1 $tmpfile2)
+set sectiondepths (cut -f2 $tmpfile2)
+set sections (cut -f3 $tmpfile2 | sed 's|^[[:space:]]*||')
+set references (seq (count $sections))
 rm $tmpfile2
 
 ################################################################################
@@ -97,18 +98,21 @@ for mdfile in (seq 0 9)*.md
         {if ($0~"#" && parstatus!=2) {print $0"\n"; parstatus=1} \
         if ($0~"#" && parstatus==2) {print "</p>\n"$0"\n"; parstatus=1} \
         if ($0!~"#" && $0 && parstatus==2) print "\n"$0; \
-        if ($0!~"#" && $0 && parstatus==1) {print "<p>"$0; parstatus=2} \
-        if (!$0 && parstatus==2) {print "</p>\n"; parstatus=1}} \
+        if ($0!~"#" && $0 && parstatus==0) {print "<p>"$0; parstatus=2} \
+        if ($0!~"#" && $0 && parstatus==1) {print "<p class=\"texttop\">"$0; parstatus=2} \
+        if (!$0 && parstatus==2) {print "</p>\n"; parstatus=0}} \
         END{if (parstatus==2) print "</p>"}' $mdfile \
     | awk -v id=$id '/^#/{sub(/^####[[:space:]]*/,"<h4 id=\"ref"id"\">") \
         sub(/^###[[:space:]]*/,"<h3 id=\"ref"id"\">") \
         sub(/^##[[:space:]]*/,"<h2 id=\"ref"id"\">") \
         sub(/^#[[:space:]]*/,"<h1 id=\"ref"id"\">") id++}1' \
     | sed -e 's|^<h\([1234]\)\(.*\)|<h\1\2</h\1>|' \
-    -e 's|\*\*\([^*]*\)\*\*|<strong>\1</strong>|g' \
-    -e 's|\*\([^*]*\)\*|<em>\1</em>|g' \
-    -e 's|__\([^_]*\)__|<strong>\1</strong>|g' \
-    -e 's|_\([^_]*\)_|<em>\1</em>|g' >> $htmlfile
+    -e 's|\~\~\([^~]*\)\~\~|<span class="st">\1</span>|g' \
+    -e 's|\~\([^~]*\)\~|<span class="u">\1</span>|g' \
+    -e 's|\*\*\([^*]*\)\*\*|<span class="b">\1</span>|g' \
+    -e 's|\*\([^*]*\)\*|<span class="i">\1</span>|g' \
+    -e 's|__\([^_]*\)__|<span class="b">\1</span>|g' \
+    -e 's|_\([^_]*\)_|<span class="i">\1</span>|g' >> $htmlfile
     echo '</body>
     </html>' >> $htmlfile
     sed -i 's|^[[:space:]]*||' $htmlfile
@@ -137,8 +141,8 @@ echo '<?xml version="1.0" encoding="utf-8"?>
 <package version="2.0" xmlns="http://www.idpf.org/2007/opf" unique-identifier="BookId">' > $contentfile
 # Write metadata section
 echo '<metadata xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:opf="http://www.idpf.org/2007/opf">
-<dc:title>$title</dc:title>
-<dc:language>$language</dc:language>
+<dc:title>'$title'</dc:title>
+<dc:language>'$language'</dc:language>
 <dc:identifier id="BookId" opf:scheme="uuid">urn:uuid:'$uuid'</dc:identifier>
 <dc:creator opf:role="aut">'$author'</dc:creator>
 <dc:publisher>'$publisher'</dc:publisher>
@@ -174,8 +178,16 @@ for content in build/OEBPS/content/(seq 9)*
 end
 echo '</spine>' >> $contentfile
 # Write guide section
+set beginning
+for file in $contentfiles
+    if test (echo $file | grep --color=never '^[1-9]')
+        set beginning $file
+        break
+    end
+end
 echo '<guide>
 <reference href="content/htmltoc.html" type="toc" title="Inhaltsverzeichnis"/>
+<reference href="content/'$beginning'.html" type="text" title="Anfang" />
 </guide>
 </package>' >> $contentfile
 
@@ -206,9 +218,6 @@ echo '<navMap>' >> $tocfile
 set depth 0
 set order 1
 set chapter 1
-set section 0
-set subsection 0
-set subsubsection 0
 for sectionnum in (seq $numsections)
     if test $sectionnum -gt 1
         switch $sectiondepths[$sectionnum]
@@ -277,19 +286,290 @@ echo '<?xml version="1.0" encoding="UTF-8"?>
 set depth 0
 set order 1
 set chapter 1
-set section 0
-set subsection 0
-set subsubsection 0
 for sectionnum in (seq $numsections)
     while test $depth -lt $sectiondepths[$sectionnum]
         set depth (expr $depth + 1)
         if test $depth -lt $sectiondepths[$sectionnum]
-            echo '<navPoint id="NCX_Chapter'$chapter'" playOrder="'$order'">' >> $tocfile
             set order (expr $order + 1)
         end
     end
-    echo '<p class="toctext"><a href="'$contentfiles[$sectionnum]'.html">'$sections[$sectionnum]'</a></p>' >> $htmltocfile
+    echo '<p class="toctext'$sectiondepths[$sectionnum]'"><a href="'$contentfiles[$sectionnum]'.html#ref'$references[$sectionnum]'">'$sections[$sectionnum]'</a></p>' >> $htmltocfile
 end
 # echo '<p class="toctext"><a href="content002.html#h2-1">Chapter 1 - Getting Started</a></p>
 echo '</body>
 </html>' >> $htmltocfile
+
+################################################################################
+# => Create stylesheet.css file if it does not exist
+################################################################################
+set stylesheet "build/OEBPS/css/stylesheet.css"
+if not test -e $stylesheet
+    echo '/* BB eBooks BoilerPlate Kindle for MOBI 7 and KF8*/
+    /* Modify as Needed */
+    /* visit us @ http://bbebooksthailand.com/developers.html */
+    /*===Reset Code===*/
+    html, body, div, applet, object, iframe, h1, h2, h3, h4, h5, h6, p, blockquote, pre, abbr, acronym, address, big, cite, code, del, dfn, em, img, ins, kbd, q, s, samp, small, strike, strong, sub, sup, tt, var, center, fieldset, form, label, legend, table, caption, tbody, tfoot, thead, article, aside, canvas, details, embed, figure, figcaption, footer, header, hgroup, menu, nav, output, ruby, section, summary, time, mark, audio, video
+    {
+    margin: 0;
+    padding: 0;
+    border: 0;
+    font-size: 100%;
+    vertical-align: baseline;
+    }
+    /* Do Not reset ol, ul, or table for MOBI 7. It blows out all styling */
+    /*===GENERAL PRESENTATION===*/
+    /*===Body Presentation and Margins===*/
+    body
+    {
+    text-align: justify;
+    line-height: 120%;
+    }
+    /*===Headings===*/
+    /* After page breaks, Kindle does not render margins above the content unless there is a file split in the package. */
+    h1
+    {
+    text-indent: 0;
+    text-align: center;
+    margin: 100px 0 0 0;
+    font-size: 2.0em;
+    font-weight: bold;
+    page-break-before: always;
+    }
+    h2
+    {
+    text-indent: 0;
+    text-align: center;
+    margin: 50px 0 0 0;
+    font-size: 1.5em;
+    font-weight: bold;
+    page-break-before: always;
+    }
+    h3
+    {
+    text-indent: 0;
+    text-align: left;
+    font-size: 1.4em;
+    font-weight: bold;
+    }
+    h4
+    {
+    text-indent: 0;
+    text-align: left;
+    font-size: 1.2em;
+    font-weight: bold;
+    }
+    h5
+    {
+    text-indent: 0;
+    text-align: left;
+    font-size: 1.1em;
+    font-weight: bold;
+    }
+    h6
+    {
+    text-indent: 0;
+    text-align: left;
+    font-size: 1.0em;
+    font-weight: bold;
+    }
+    /*===Paragraph Elements===*/
+    /* Margins are usually added on the top, left, and right, but not on the bottom to prevent Kindle not collapsing white space properly */
+    /*firstline indent for fiction*/
+    p
+    {
+    text-indent: 1.25em;
+    margin: 0;
+    }
+    /*block indent for non-fiction*/
+    /*
+    p
+    {
+    text-indent: 0;
+    margin: 1.0em 0 0 0;
+    }
+    */
+    /* for centered text and element wrappers on images*/
+    p.centered
+    {
+    text-indent: 0;
+    margin: 1.0em 0 0 0;
+    text-align: center;
+    }
+    /* section Breaks (can use centered-style for non-fiction) */
+    p.centeredbreak
+    {
+    text-indent: 0;
+    margin: 1.0em 0 1.0em 0;
+    text-align: center;
+    }
+    /* First sentence in chapters following heading */
+    p.texttop
+    {
+    margin: 1.5em 0 0 0;
+    text-indent: 0;
+    }
+    /* 1st level TOC */
+    p.toctext1
+    {
+    margin: 0 0 0 1.5em;
+    text-indent: 0;
+    text-align: left;
+    }
+    /* 2nd level TOC */
+    p.toctext2
+    {
+    margin: 0 0 0 2.5em;
+    text-indent: 0;
+    text-align: left;
+    }
+    /* 3rd level TOC */
+    p.toctext3
+    {
+    margin: 0 0 0 3.5em;
+    text-indent: 0;
+    text-align: left;
+    }
+    /* 4th level TOC */
+    p.toctext4
+    {
+    margin: 0 0 0 4.5em;
+    text-indent: 0;
+    text-align: left;
+    }
+    /*==IMAGES==*/
+    /*===IN-LINE STYLES===*/
+    span.i
+    {
+    font-style: italic;
+    }
+    span.b
+    {
+    font-weight: bold;
+    }
+    span.u
+    {
+    text-decoration: underline;
+    }
+    span.st
+    {
+    text-decoration: line-through;
+    }
+    /*==in-line combinations==*/
+    /* Using something like <span class="i b">... may seem okay, but it causes problems on the Kindle */
+    span.ib
+    {
+    font-style: italic;
+    font-weight: bold;
+    }
+    span.iu
+    {
+    font-style: italic;
+    text-decoration: underline;
+    }
+    span.bu
+    {
+    font-weight: bold;
+    text-decoration: underline;
+    }
+    span.ibu
+    {
+    font-style: italic;
+    font-weight: bold;
+    text-decoration: underline;
+    }
+    /* Superscripted Footnote Text */
+    .footnote
+    {
+    vertical-align: super;
+    font-size: 0.75em;
+    text-decoration: none;
+    }
+    /*==KF8 specific here ==*/
+    @media amzn-kf8{
+    span.dropcap {
+    font-size: 300%;
+    font-weight: bold;
+    height: 1em;
+    float: left;
+    margin: -0.2em 0.1em 0 0.1em;
+    }
+    p.clearit
+    {
+    clear: both;
+    }
+    ol, ul, li, dl, dt, dd
+    {
+    margin: 0;
+    padding: 0;
+    border: 0;
+    font-size: 100%;
+    vertical-align: baseline;
+    }
+    /*==Lists ==*/
+    ul
+    {
+    margin: 1em 0 0 2em;
+    text-align: left;
+    }
+    ol
+    {
+    margin: 1em 0 0 2em;
+    text-align: left;
+    }
+    table
+    {
+    border-collapse: collapse;
+    border-spacing: 0;
+    margin: 1.0em auto;
+    }
+    tr, th, td
+    {
+    margin: 0;
+    padding: 2px;
+    border: 1px solid black;
+    font-size: 100%;
+    vertical-align: baseline;
+    }
+    } /* End KF8 Specific Styles */
+    /*==e-ink Kindle Specific==*/
+    @media amzn-mobi{
+    /* pseudo dropcaps for e-ink Kindles */
+    span.dropcap {
+    font-size: 1.5em;
+    font-weight: bold;
+    }
+    } /* End e-ink Kindle Specific Styles */
+    /*==eBook Specific Formatting Below Here==*/' > $stylesheet
+    sed -i 's|^[[:space:]]*||' $stylesheet
+end
+
+################################################################################
+# => Copy cover.jpg
+################################################################################
+set coverimage "build/OEBPS/images/cover.jpg"
+if not test -e $coverimage
+    cp cover.jpg $coverimage
+end
+
+################################################################################
+# => Create epub file
+################################################################################
+cd build
+set epubfile (echo $title | sed 's|[[:space:]]|_|g')".epub"
+set mobifile (echo $title | sed 's|[[:space:]]|_|g')".mobi"
+if test -e $epubfile
+    rm $epubfile
+end
+if test -e $mobifile
+    rm $mobifile
+end
+zip -0Xq $epubfile mimetype
+zip -Xr9Dq $epubfile *
+epubcheck $epubfile
+
+################################################################################
+# => Create mobi file if kindlegen is detected
+################################################################################
+if type kindlegen
+    kindlegen -c1 $epubfile
+end
